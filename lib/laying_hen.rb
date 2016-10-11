afb5cf8f00592484f6788631ddecdb95c7d91734
codeamac01 <codeamac01@gmail.com>
2016-10-11 17:34:47 -0500

    TDD completado

--- lib/laying_hen.rb
+++ lib/laying_hen.rb
@@ -1,13 +1,106 @@
 class LayingHen
+  attr_reader :age, :eggs
+
   def initialize
+    @age = 0
+    @eggs = 0
+    @hatched_hours = 0
   end
 
   # Ages the hen one month, and lays 4 eggs if the hen is older than 3 months
   def age!
-    
-  end  
-end
+    @age += 1
+    if @age > 3 && @age < 10
+      4.times { @eggs += 1 }
+    end  
+  end
+
+  # Returns "true" if the hen has laid any eggs, "false" otherwise
+  def any_eggs?
+    @eggs > 0? true : false
+  end
+
+  # Returns an Egg if there are any
+  # Raises a NoEggsError otherwise
+  def pick_an_egg!
+    raise NoEggsError, "The hen has not layed any eggs" unless self.any_eggs?
+    @eggs -= 1
+    Egg.new(@hatched_hours)
+  end
+
+  # Returns "true" if the hen can't laid eggs anymore because of its age, "false" otherwise.
+  # The max age for a hen to lay eggs is 10 
+  def old?
+    @age < 10 ? false : true 
+  end
+
+  def increase_hatched_hour(hours)
+    @hatched_hours = hours 
+  end
+
+end#class LayingHen
 
 class Egg
+        #attr_reader :hatched_hours
+  # Initializes a new Egg with hatched hours +hatched_hours+
+  def initialize(hatched_hours)
+    @hatched_hours = hatched_hours
+  end
 
-end
+  # Return +true+ if hatched hours is greater than 3, +false+ otherwise
+  def rotten?
+    @hatched_hours > 3 ? true : false
+  end
+end#Class Egg
+
+class NoEggsError < StandardError; end
+
+
+
+
+
+# hen = LayingHen.new
+# basket = []
+# rotten_eggs = 0
+#       # puts "*"*5
+# hen.age! until hen.any_eggs? #Incrementa la edad hasta que ponga huevos
+#       # puts "*"*5
+# puts "Hen is #{hen.age} months old, its starting to laid eggs."
+# puts ""
+#       # puts "*"*5
+# until hen.old?
+#   #The time we take to pick up the eggs
+#   hours = rand(5)
+#       # puts hours
+#   hen.increase_hatched_hour(hours)
+
+
+#   while hen.any_eggs?
+#     egg = hen.pick_an_egg!      
+
+#       # puts egg.rotten?
+#       # puts egg.hatched_hours
+#     if egg.rotten?
+#       rotten_eggs += 1
+#     else
+#       basket << egg
+#     end
+#   end
+
+#   puts "Month #{hen.age} Report"
+#   puts "We took #{hours} hour(s) to pick the eggs"
+#   puts "Eggs in the basket: #{basket.size}"
+#   puts "Rotten eggs: #{rotten_eggs}"
+#   puts ""
+
+#   # Age the hen another month
+#   hen.age!
+# end
+#       # puts "*"*10
+# puts "The hen is old, she can't laid more eggs!"
+# puts "The hen laid #{basket.size + rotten_eggs} eggs"
+# if rotten_eggs == 0
+#   puts "CONGRATULATIONS NO ROTTEN EGGS" 
+# else
+#   puts "We pick to late #{rotten_eggs} eggs so they become rotten"
+# end
--- spec/laying_hen_spec.rb
+++ spec/laying_hen_spec.rb
@@ -2,8 +2,114 @@ require 'laying_hen'
 
 describe LayingHen do
   # LayingHen tests here
-end
+  let(:hen) { LayingHen.new }
+
+  subject { hen }
+  it { is_expected.to respond_to(:age) }
+  it { is_expected.to respond_to(:eggs) }
+
+  describe "#initialize" do
+    context "with valid input" do
+      it "creates a new LayingHen" do
+        expect(hen).to be_an_instance_of LayingHen
+      end
+    end
+  end
+
+  describe "#age!" do
+    it "increases age" do
+      expect { hen.age! }.to change(hen, :age).from(0).to(1)
+    end
+
+    context "when is younger than 3 months" do
+      before { 2.times { hen.age! } }
+      it "eggs should be 0" do
+        hen.eggs.should be(0)
+      end
+    end
+
+    context "when is older than 3 months" do
+      before { 4.times { hen.age! } }
+      it "eggs should be 4" do
+        hen.eggs.should be(4)
+      end
+    end
+  end
+
+  describe "#any_eggs?" do
+    context "hen has laid any eggs" do
+      before { 6.times { hen.age! } }
+      it { hen.any_eggs?.should be true }
+    end
+
+    context "hen has not laid any eggs" do
+      it { hen.any_eggs?.should be false }      
+    end
+  end
+
+  describe "#pick_an_egg!" do
+    context "hen has not laid any eggs" do
+      it { expect { hen.pick_an_egg! }.to raise_error(NoEggsError) }
+    end
+
+    context "hen has laid any eggs" do
+      before { 4.times { hen.age! } }
+      it "decreases eggs" do
+        expect { hen.pick_an_egg! }.to change(hen, :eggs).from(4).to(3)
+      end
+
+      it "return instance of Egg" do 
+        hen.pick_an_egg!.should be_an_instance_of Egg
+      end
+    end
+  end
+
+  describe "#old?" do
+    context "when hen is younger than 10 months" do
+      it { hen.old?.should be false }
+    end
+
+    context "when hen is older than 10 months" do
+      before { 10.times { hen.age! } }
+      it { hen.old?.should be true }
+    end    
+  end
+
+  describe "#increase_hatched_hour" do
+    it { hen.increase_hatched_hour(3).should eq(3) }
+  end
+
+end# describe LayingHen
 
 describe Egg do
   # Egg tests here
-end
+  let(:hatched_hours) { 4 }
+  let(:egg) { Egg.new(hatched_hours) }
+
+  describe "#initialize" do
+    context "with valid input" do
+      it "creates a new Egg" do
+        expect(Egg.new(hatched_hours)).to be_an_instance_of Egg
+      end
+    end
+
+    context "with invalid input" do
+      let(:egg) { Egg.new("a") }
+      it "#rotten? raise error" do
+        expect{egg.rotten?}.to raise_error(ArgumentError)
+      end
+    end
+  end 
+
+  describe "#rotten?" do
+    context "hatched_hours are greater than 3 hours" do
+      it { egg.rotten?.should be true}
+    end
+
+    context "hatched_hours are less than 3 hours" do
+      let(:egg) { Egg.new(2) }
+      it { egg.rotten?.should be false}
+    end
+  end 
+
+end# describe Egg
